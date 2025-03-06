c2626129081ad1db5344ae9c30c580e5c468b70b
ceastld <1950946176@qq.com>
2025-03-06 16:28:56 +0800

    1

--- comp.py
+++ comp.py
@@ -1,9 +1,12 @@
 from functools import cached_property
 import os
+from pathlib import Path
 import shlex
+import shutil
 import subprocess
 
 import numpy as np
+from tqdm import tqdm
 from expdataloader import *
 from expdataloader.utils import img_grid
 
@@ -66,16 +69,23 @@ class CompLoaderImage(RowDataLoader):
 
     @cached_property
     def comp_cross_data(self):
-        return [
-            ("323_EXP-5-mouth_cam_222200047", "000236.jpg"),
-            ("321_EXP-8-jaw-1_cam_222200042", "000124.jpg"),
-            ("324_EXP-8-jaw-1_cam_222200039", "000044.jpg"),
-            ("325_EXP-8-jaw-1_cam_222200045", "000112.jpg"),
-            ("332_EXP-8-jaw-1_cam_222200038", "000080.jpg"),
-            ("369_EXP-8-jaw-1_cam_222200048", "000092.jpg"),
-            ("370_EXP-2-eyes_cam_221501007", "000096.jpg"),
-            ("375_EXP-2-eyes_cam_222200036", "000100.jpg"),
-        ]
+        data = []
+        with open("comp_cross.txt") as f:
+            lines = f.readlines()
+            for line in lines:
+                path = Path(line.strip())
+                data.append((path.parent.parent.name, path.name))
+        return data
+        # return [
+        #     ("323_EXP-5-mouth_cam_222200047", "000236.jpg"),
+        #     ("321_EXP-8-jaw-1_cam_222200042", "000124.jpg"),
+        #     ("324_EXP-8-jaw-1_cam_222200039", "000044.jpg"),
+        #     ("325_EXP-8-jaw-1_cam_222200045", "000112.jpg"),
+        #     ("332_EXP-8-jaw-1_cam_222200038", "000080.jpg"),
+        #     ("369_EXP-8-jaw-1_cam_222200048", "000092.jpg"),
+        #     ("370_EXP-2-eyes_cam_221501007", "000096.jpg"),
+        #     ("375_EXP-2-eyes_cam_222200036", "000100.jpg"),
+        # ]
 
     def comp_cross_img(self):
         img_path_list = []
@@ -84,9 +94,9 @@ class CompLoaderImage(RowDataLoader):
                 row = loader.get_row(data_name)
                 img_path = os.path.join(row.frames_dir, img_name)
                 img_path_list.append(img_path)
-        img_array = np.array(img_path_list).reshape(-1,7)
-        img_grid(img_array, save_path="comp_cross.jpg")
-        
+        img_array = np.array(img_path_list).reshape(-1, 7)
+        img_grid(img_array, save_path=os.path.join(self.comp_cross_dir, "comp_cross.jpg"))
+
     def comp_cross_img_rs(self):
         img_path_list = []
         for data_name, img_name in self.comp_cross_data:
@@ -94,9 +104,30 @@ class CompLoaderImage(RowDataLoader):
             img_path_list.append(row.source_img_path)
             img_path = os.path.join(row.target.imgs_dir, change_extension(img_name, row.target.img_ext))
             img_path_list.append(img_path)
-        img_array = np.array(img_path_list).reshape(-1,2)
-        img_grid(img_array, save_path="comp_cross_rs.jpg")
-                
+        img_array = np.array(img_path_list).reshape(-1, 2)
+        img_grid(img_array, save_path=os.path.join(self.comp_cross_dir, "comp_cross_rs.jpg"))
+
+    @cached_property
+    def comp_cross_dir(self):
+        return get_sub_dir("data", "comp_cross_v2")
+
+    def comp_cross_img_all(self):
+        save_dir = self.comp_cross_dir
+        for data_name, img_name in tqdm(self.comp_cross_data):
+            row = self.get_row(data_name)
+            paths = [(row.source_img_path, "source"), (os.path.join(row.target.imgs_dir, change_extension(img_name, row.target.img_ext)), "driving")]
+            for loader in self.all_loaders:
+                row = loader.get_row(data_name)
+                paths.append((os.path.join(row.frames_dir, img_name), loader.name))
+            for img_path, subdir in paths:
+                os.makedirs(os.path.join(save_dir, subdir), exist_ok=True)
+                save_path = os.path.join(save_dir, subdir, f"{data_name}+{img_name}")
+                shutil.copyfile(img_path, save_path)
+
+    def comp_cross(self):
+        self.comp_cross_img()
+        self.comp_cross_img_rs()
+        self.comp_cross_img_all()
 
 
 def comp():
@@ -105,9 +136,13 @@ def comp():
     # loader.run_video(loader.all_data_rows[0])
     loader.run_all()
 
+
 def main():
     loader = CompLoaderImage("comp")
-    loader.comp_cross_img_rs()
-    
+    loader.comp_cross()
+    # for data in loader.comp_cross_data:
+    #     print(data)
+
+
 if __name__ == "__main__":
     main()
